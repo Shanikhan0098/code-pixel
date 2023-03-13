30fb88ad93c0c326a9fa508cb9ccae294b6e2bdc
zeeshan <shanikhan0098>
2023-03-12 23:34:58 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let show = document.getElementById('show');
+show.addEventListener('mouseover', function () {
+    let apear = document.getElementById('apear');
+    apear.style.display = 'block'
+    show.style.backgroundColor = 'black'
+})
